5180dc18fe0020b5c6f7c6c9b4513b6c970f4bcd
Marco Antonio Gonzalez Junior <marco.gonzalez@jussi.com.br>
2015-03-04 17:44:13 -0300

    Java endpoint

--- hello.rb
+++ hello.rb
@@ -8,7 +8,7 @@ post '/java' do
   puts params.inspect
   puts params[:file]
   tmpfile = params[:file][:tempfile]
-  directory = './java'
+  directory = '/app/java'
   name = params[:file][:filename]
   path = File.join(directory, name)
   File.open(path, "wb") { |f| f.write(tmpfile.read) }
