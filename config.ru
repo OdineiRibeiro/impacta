682ef4992bab3a3f449ef7b43c4f72ee5447ed95
Marco Antonio Gonzalez Junior <marco.gonzalez@jussi.com.br>
2015-03-05 17:20:41 -0300

    Java and JS tests.

--- config.ru
+++ config.ru
@@ -1,3 +1,3 @@
-require './hello'
+require './impacta'
 $stdout.sync = true
 run Sinatra::Application
--- impacta.rb
+++ impacta.rb
@@ -1,8 +1,24 @@
 require 'sinatra'
+require 'v8'
+
+configure do
+  set :port, 8080
+end
 
 get '/' do
-  "Hello World!"
+  "Faculdade Impacta de Tecnologia"
 end
+
+get '/code' do
+  
+  erb :code
+end
+
+post '/js' do
+  cxt = V8::Context.new
+  cxt.eval('7 * 6')
+end
+
 
 post '/java' do
   puts params.inspect
